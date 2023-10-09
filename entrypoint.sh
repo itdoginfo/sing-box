97c451fcaf3c0c7a89e343ee4e6bd690b8b90aad
itdoginfo <koalav42@gmail.com>
2023-10-09 18:41:28 +0300

    Check workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,4 @@ if [ ! -f /etc/sing-box/config.json ]; then
     mustpl -d '{"METHOD": "${METHOD:-2022-blake3-aes-128-gcm}", "PASS": "${PASS:-SET_PASSWORD}", "PORT": "${PORT:-443}"}' -o /etc/sing-box/config.json /opt/config-template.json
 fi
 
-
 exec "$@"
